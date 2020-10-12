824f903cab9f725aa26c791a99704030be816a31
Tetat Chulchue <tetat.chulchue@gmail.com>
2020-10-12 13:48:21 +0700

    Add Welcome Screen
    + Logo placeholder
    + Welcome Screen (Unfinshed)

---
+++ app/components/Logo.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
+const { width, height } = Dimensions.get('window');
+
+class Logo extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Image
+                    source={require('../assets/img/logo_placeholder.png')}
+                    style={styles.logo}
+                />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        height: height * 0.67
+    },
+    logo: {
+        flex: 2,
+        width: width * 0.9,
+        resizeMode: 'contain',
+    },
+});
+
+
+export default Logo;
---
+++ app/screen/Welcome.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Logo from '../components/Logo'
+
+class Welcome extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null
+        }
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.logo}>
+                    <Logo/>
+                </View>
+                <Text style={styles.title}>
+                    Thoth's Riddle
+                </Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+    },
+    title: {
+        flex: 1,
+        fontSize: 30,
+        fontWeight: 'bold',
+    },
+    logo: {
+        flex: 2
+    }
+});
+
+export default Welcome;
